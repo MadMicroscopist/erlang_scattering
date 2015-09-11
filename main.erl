9da920f86fcee5366f27b92bd853fcd05fce784f
MadMicroscopist <madmicroscopist@gmail.com>
2015-09-11 15:36:58 +0300

    v0.1.2.2015.09.11 Now "watcher" processes are spawned instead of loop (electron).

--- main.erl
+++ main.erl
@@ -1,28 +1,39 @@
 -module(main).
--export([start/1, waiting/1, counter/2, loop/0, stop/1]).
+-export([start/1, waiting/1, counter/2, electron/1, watcher/0, stop/1]).
 
+%start simulation for N_e electrons
 start(N_e) ->
     register(server, spawn(main, waiting, [N_e])).
 
 waiting(N_e) ->
     spawn(main, counter, [N_e,[]]),
     receive
-        {message, Msg} ->
-            io:format("Recieved message ~p~n", [Msg]);
         {stop, List} ->
-            io:format("Process list is ~p~n", [List]),
+            io:format("Spawned ~p~n", [List]),
             unregister(server),
-            ok
+            ok;
+        _Message ->
+            _Message
     end.
-
-stop(List) ->
-    server ! {stop, List}.
 
 counter(0, List) ->
     stop(List);
 counter(N_e, List) ->
-    Pid = spawn(main, loop, []),
+    Pid = spawn(main, watcher, []),
     counter(N_e-1, [{N_e, Pid}|List]).
 
-loop() ->
-    server ! {message, "hello"}.
+watcher() ->
+    spawn(main, electron, [self()]),
+    receive
+        {Pid, Msg} ->
+            io:format("Message ~p received from ~p~n", [Msg, Pid]);
+        _Other ->
+            _Other
+        end.
+
+electron(Pid) ->
+    Pid ! {self(), "hello"}.
+
+
+stop(List) ->
+    server ! {stop, List}.
