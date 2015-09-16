c01f2bbf1b868f8457c8c9ae3c98c4c2f6a02025
MadMicroscopist <madmicroscopist@gmail.com>
2015-09-16 15:05:09 +0300

    v0.3.1.2015.09.16 module(file_searcher) 	-recives a file and makes a list of lists of every line; 	-each element in a list is a binary.

--- file_searcher.erl
+++ file_searcher.erl
@@ -1,20 +1,13 @@
 -module(file_searcher).
--export([read_file/2]).
+-export([read_file/2, read_each_line/2]).
+-define(Punctuation,"[ | , | \\ . | ; | : | \\ t | \\ n | \\ ( | \\ ) ] + " ).
 
 read_file(Name, Mode) ->
     {ok, Device} = file:open(Name, Mode),
-    read_charachters(Device, []).
-    %read_each_line(Device, []).
+    read_each_line(Device, []).
 
 read_each_line(Device, Accum) ->
     case io:get_line(Device, "") of
-        eof  -> file:close(Device), array:from_list(lists:reverse(Accum));
-        Line -> read_each_line(Device, [Line | Accum])
-    end.
-
-read_charachters(Device, Accum) ->
-    case file:read(Device, 40) of
-        newline -> read_charachters(Device, [[]|Accum]);
-        eof -> file:close(Device), lists:reverse(Accum);
-        Char -> read_charachters(Device, [Char|Accum])
+        eof  -> file:close(Device), lists:reverse(Accum);
+        Line -> read_each_line(Device, [re:split(Line, ?Punctuation) | Accum])
     end.
