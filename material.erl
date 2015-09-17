1f2911180ff5ff21f7c501a1444a8533f32261db
MadMicroscopist <madmicroscopist@gmail.com>
2015-09-17 12:34:51 +0300

    v0.3.3.2015.09.17 module(material) 	-function fill_cs_table/2 REALLY makes from data a list of turples {{Energy,Angle},Value}.

--- material.erl
+++ material.erl
@@ -1,30 +1,46 @@
 -module(material).
 -export([fill_cs_table/2]).
+%-import(file_searcher, [read_file/2]).
 -include("electron.hrl").
+-define(Punctuation,"[ | , | \\ . | ; | : | \\ t | \\ n | \\ ( | \\ ) ] + " ).
+
+read_file(Name, Mode) ->
+    {ok, Device} = file:open(Name, Mode),
+    read_each_line(Device, []).
+
+read_each_line(Device, Accum) ->
+    case io:get_line(Device, "") of
+        eof  -> file:close(Device), lists:reverse(Accum);
+        Line -> read_each_line(Device, [re:split(Line, ?Punctuation) | Accum])
+    end.
 
 fill_cs_table(FileName, Mode) ->
-    List = file_searcher:read_file(FileName, Mode),
+    List = read_file(FileName, Mode),
     mixer(List, []).
 
 mixer([H|T], List) ->
     case T of
-        [ ] ->  mixer2(H,List, []);
-        [A|B] -> mixer([H|B], mixer1(A, List))
+        [ ] ->  mixer2(H, lists:reverse(List), []);
+        [A|B] -> mixer([H|B], [mixer1(A)|List])
     end.
 
-mixer1([A|B], List) ->
+mixer1([A|B]) ->
     case B of
-        [] -> List;
-        [C|T]  -> mixer1([A|T],[{A,C}|List] )
+        [] -> [];
+        _  -> {A, B}
     end.
 
-mixer2([Ha|Ta], [Ht|Tt], List) ->
-    case Ta of
-        [] -> [mixer3(Tt,[])|List];
-        _  -> mixer2(Ta, Tt, [{Ha, Ht}|List])
+mixer2(A, [Ht|Tt], List) ->
+    case Tt of
+        []  -> lists:reverse(List);
+        %[] -> mixer2(A, [Ht], [mixer3(A, Ht, [])|List]);
+        _  -> mixer2(A, Tt, [mixer3(A, Ht, [])|List])
     end.
-mixer3([{A, B}|T],List) ->
+mixer3([H|T],{E, [Hc|Tc]}, List) ->
     case T of
-        [] -> [{{full, A},B}|List];
-        _  -> mixer3(T,[{{full, A},B}|List])
+        [] -> lists:reverse([mixer4(E,Tc),{{E,H},Hc}|List]);
+        _  -> mixer3( T, {E, Tc}, [{{E,H},Hc}|List])
     end.
+
+mixer4(E,T) ->
+    {{E, full}, T}.
