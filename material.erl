ba0f5360ab0d8ea11989577437c1d475b076b26b
MadMicroscopist <madmicroscopist@gmail.com>
2015-09-29 12:24:47 +0300

    v0.4.0.2015.09.29 -module(mixer) 	-add a separate module for "mixer" functions; 	-this functions makes a list of turples from list of lists; -module(material) 	-corresponding change of mixer function call.

--- material.erl
+++ material.erl
@@ -16,31 +16,4 @@ read_each_line(Device, Accum) ->
 
 fill_cs_table(FileName, Mode) ->
     List = read_file(FileName, Mode),
-    mixer(List, []).
-
-mixer([H|T], List) ->
-    case T of
-        [ ] ->  mixer2(H, lists:reverse(List), []);
-        [A|B] -> mixer([H|B], [mixer1(A)|List])
-    end.
-
-mixer1([A|B]) ->
-    case B of
-        [] -> [];
-        _  -> {A, B}
-    end.
-
-mixer2(A, [Ht|Tt], List) ->
-    case Tt of
-        []  -> lists:reverse(List);
-        %[] -> mixer2(A, [Ht], [mixer3(A, Ht, [])|List]);
-        _  -> mixer2(A, Tt, [mixer3(A, Ht, [])|List])
-    end.
-mixer3([H|T],{E, [Hc|Tc]}, List) ->
-    case T of
-        [] -> lists:reverse([mixer4(E,Tc),{{E,H},Hc}|List]);
-        _  -> mixer3( T, {E, Tc}, [{{E,H},Hc}|List])
-    end.
-
-mixer4(E,T) ->
-    {{E, full}, T}.
+    mixer:mixer(List). %function, wich makes a list of turples from list of lists.
---
+++ mixer.erl
@@ -0,0 +1,32 @@
+-module(mixer).
+-export([mixer/1]).
+
+mixer(Main) ->
+    mixer3(hd(Main), mixer1(tl(Main),[]), []).
+
+mixer1(Tail, List) ->
+    case Tail of
+        [] -> lists:reverse(List);
+        _  -> mixer1(tl(Tail), [mixer2(hd(Tail))|List])
+    end.
+
+mixer2(Energy) ->
+    {hd(Energy), lists:delete(lists:last(Energy), tl(Energy)), lists:last(Energy)}.
+
+mixer3(Angle, List1, List) ->
+    case List1 of
+        [] -> lists:reverse(List);
+        _  -> mixer3(Angle, tl(List1), [mixer4(Angle, hd(List1))|List])
+    end.
+
+mixer4(Angle, {E, List2, S_full}) ->
+    lists:reverse([{E, full, S_full}|mixer5(E, lists:zip(Angle, List2), [])]).
+
+mixer5(E, List3, List) ->
+    case List3 of
+        [] -> List;
+        _  -> mixer5(E, tl(List3), [mixer6(E, hd(List3))|List] )
+    end.
+
+mixer6(E, {Angle, Sigma}) ->
+    {E, Angle, Sigma}.
