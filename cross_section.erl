976f15e0814154e9e4d3c5a9c98b97b6fbe11a7d
MadMicroscopist <madmicroscopist@gmail.com>
2015-10-04 00:58:35 +0300

    v0.6.0.2015.10.03 -module(cross_section) 	-functions which regards to tables were deleted.

--- cross_section.erl
+++ cross_section.erl
@@ -1,24 +1,7 @@
 -module(cross_section).
--export([create_tables/1, close_tables/0, fill_cs_table/2, get_cs/1]).
+-export([fill_cs_table/1]).
 
-create_tables(FileName) ->
-    ets:new(csMott, [named_table]),
-    dets:open_file(usrDisk, [{file, FileName}]).
-
-close_tables() ->
-    ets:delete(csMott),
-    dets:close(usrDisk).
-
-add_cs(CS) ->
-    ets:insert(csMott, CS),
-    dets:insert(usrDisk, CS),
-    ok.
-
-get_cs({Energy, Angle}) ->
-    case ets:lookup(csMott, {Energy, Angle}) of
-        [{{Energy, Angle}, Value}] -> {ok, Value};
-        [] -> {error, instance}
-    end.
+fill_cs_table(FileName) -> fill_cs_table(FileName, read).
 
 read_file(Name, Mode) ->
     {ok, Device} = file:open(Name, Mode),
@@ -33,18 +16,4 @@ read_each_line(Device, Accum) ->
 fill_cs_table(FileName, Mode) ->
     List = read_file(FileName, Mode),
     %mixer:mixer(List). %function, wich makes a list of turples from list of lists.
-    Normal_list = probability:calculate(mixer:mixer(List)),
-    filling(Normal_list),
-    lists:reverse(tl(lists:reverse(Normal_list))).
-
-filling(List) ->
-    case List of
-        [] -> ok;
-        _  -> fill_line(hd(List)), filling(tl(List))
-    end.
-
-fill_line(List) ->
-    case List of
-        [] -> ok;
-        _  -> add_cs({{element(1, hd(List)), element(2, hd(List))}, element(3, hd(List))}), fill_line(tl(List))
-    end.
+    probability:calculate(mixer:mixer(List)).
--- searcher.erl
+++ searcher.erl
@@ -1,5 +1,6 @@
 -module(searcher).
 -export([search/3]).
+
 search(Energy, Value, List) ->
     search_energy(Energy, Value, List, hd(List)).
 
@@ -12,8 +13,9 @@ search_energy(Energy, Value, List, E0) ->
         true -> search_energy(Energy, Value, tl(List), hd(List))
     end.
 
-%function for case of exact energy matching
-search_value(List, Value) -> search_value(List, Value, 0, 0).
+%% function for case of exact energy matching
+search_value(List, Value) ->
+    search_value(List, Value, 0, 0).
 
 search_value(List, Value, A0, V0) ->
     Temp = element(3, hd(List)),
@@ -23,11 +25,13 @@ search_value(List, Value, A0, V0) ->
         true  -> search_value(tl(List), Value, element(2, hd(List)), Temp)
     end.
 
-%function for case of middle value
+%% function for case of middle value
 search_value_1(Min, Max, Value, Energy) ->
     A = search_value(Min, Value, 0, 0),
     B = search_value(Max, Value, 0, 0),
-    middle(A, element(1, hd(Min)), B, element(1, hd(Max)), Energy).
+    {MinE, _, _} = hd(Min),
+    {MaxE, _, _} = hd(Max),
+    middle(A, MinE, B, MaxE, Energy).
 
 middle(Angle_min, Value_min, Angle_max, Value_max, Value) ->
     (Value*(Angle_max - Angle_min) + Angle_min*Value_max - Angle_max*Value_min)/(Value_max-Value_min).
